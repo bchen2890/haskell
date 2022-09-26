660c5c7fd507bb442efc558b4613f6eea66b1b4b
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-09-26 20:42:30 +0200

    Arithmetic

---
+++ arithmetic.hs
@@ -0,0 +1,15 @@
+main = do
+    putStrLn "2 + 1 ="
+    print $ 2 + 1
+    putStrLn "5 - 3 ="
+    print $ 5 - 3
+    putStrLn "4 * 5 ="
+    print $ 4 * 5
+    putStrLn "22 / 2 ="
+    print $ 22 / 2
+    putStrLn "(30/6) + 4 - (2 * 3) ="
+    print $ (30/6) + 4 - (2 * 3)
+    putStrLn "succ 9 ="
+    print $ succ 9
+    putStrLn "min -2 1 ="
+    print $ min (-2) 1  
